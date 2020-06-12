ef2f0f30ca98c4e3a23a2340ee6cabb4d1ae81d6
Adam Weissman <47704263+AdamWeissman@users.noreply.github.com>
2020-06-11 23:11:47 -0400

    swithcboard.js and app.css and index.html mostly done

---
+++ switchboard.js
@@ -0,0 +1,51 @@
+class SwitchBoard {
+  constructor () {
+    this.main = document.querySelector('main')
+    this.theSetup = document.getElementById('theSetup')
+    this.theSlice = document.getElementById('theSlice')
+    
+    this.act1 = document.getElementById('act1')
+    this.act2 = document.getElementById('act2')
+    this.act3 = document.getElementById('act3')
+    this.act4 = document.getElementById('act4')
+    this.act5 = document.getElementById('act5')
+
+    this.rowAlabel = document.getElementById('unityAlabel')
+    this.rowBlabel = document.getElementById('unityBlabel')
+    this.rowClabel = document.getElementById('unityClabel')
+    this.rowDlabel = document.getElementById('unityDlabel')
+    this.rowElabel = document.getElementById('unityElabel')
+
+    this.rowAcolumn1 = document.getElementById('unityA1o5a_col1')
+    this.rowAcolumn2 = document.getElementById('unityA2o5a_col2')
+    this.rowAcolumn3 = document.getElementById('unityA3o5a_col3')
+    this.rowAcolumn4 = document.getElementById('unityA4o5a_col4')
+    this.rowAcolumn5 = document.getElementById('unityA5o5a_col5e')
+
+    this.rowBcolumn1 = document.getElementById('unityB1o3a_col1')
+    this.rowBcolumn2 = document.getElementById('unityB2o3a_col2')
+    this.rowBcolumn3 = document.getElementById('unityB3o3a_col3c')
+    this.rowBcolumn4 = document.getElementById('unityB1o2b_col4')
+    this.rowBcolumn5 = document.getElementById('unityB2o2b_col5e')
+
+    this.rowCcolumn1 = document.getElementById('unityC1o2a_col1')
+    this.rowCcolumn2 = document.getElementById('unityC2o2a_col2b')
+    this.rowCcolumn3 = document.getElementById('unityC1o3b_col3')
+    this.rowCcolumn4 = document.getElementById('unityC2o3b_col4')
+    this.rowCcolumn5 = document.getElementById('unityC3o3b_col5e')
+
+    this.rowDcolumn1 = document.getElementById('unityD1o1a_col1a')
+    this.rowDcolumn2 = document.getElementById('unityD1o2b_col2')
+    this.rowDcolumn3 = document.getElementById('unityD2o2b_col3c')
+    this.rowDcolumn4 = document.getElementById('unityD1o1c_col4d')
+    this.rowDcolumn5 = document.getElementById('unityD1o1d_col5e')
+
+    this.rowEcolumn1 = document.getElementById('unityE1o1a_col1a')
+    this.rowEcolumn2 = document.getElementById('unityE1o1b_col2b')
+    this.rowEcolumn3 = document.getElementById('unityE1o2c_col3')
+    this.rowEcolumn4 = document.getElementById('unityE2o2c_col4d')
+    this.rowEcolumn5 = document.getElementById('unityE1o1d_col5e')
+  }
+}
+
+const switchboard = new SwitchBoard()
---
+++ talker.js
@@ -0,0 +1,48 @@
+class Talker {
+  constructor () {
+    this.synth = window.speechSynthesis;
+  }
+
+  speak(text) {  
+    let utterThis;
+    if (this.synth.speaking) {
+          console.error('speechSynthesis.speaking');
+          return;
+      }
+      if ([text] !== '') {
+        const voices = this.synth.getVoices();
+        utterThis = new SpeechSynthesisUtterance([text]) 
+        try {
+          utterThis.pitch = 1.0;
+          utterThis.rate = .9;
+          utterThis.voice = voices[49]}
+        catch {
+          utterThis.pitch = 1.7;
+          utterThis.rate = .7;
+          utterThis.voice = voices[0]}
+        //leaves voice as default
+      }
+      
+      utterThis.onend = function (event) {
+          console.log('SpeechSynthesisUtterance.onend');
+      } 
+      utterThis.onerror = function (event) {
+          console.error('SpeechSynthesisUtterance.onerror');
+          console.log(utterThis)
+      }
+  
+      this.synth.speak(utterThis);
+      console.log(utterThis)
+    }
+
+    wait(ms) {
+      let start = new Date().getTime();
+      let end = start;
+      while(end < start + ms) {
+        end = new Date().getTime();
+     }
+   }
+
+}
+
+const talker = new Talker()
