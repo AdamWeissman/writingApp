69d93df8f41748eaba83bbabc0d66bec308c4630
Adam Weissman <47704263+AdamWeissman@users.noreply.github.com>
2020-06-27 20:05:45 -0400

    need to connect the switchboard to what's in the story

--- main.js
+++ main.js
@@ -20,19 +20,21 @@ class runTimeThing {
   
   questions = [
     () => this.questioner("Hi, What's your name?", "firstName", "Nice to meet you "),
-    () => this.questioner("What do you want to call this project?  Try to think of something that sounds cool and captures the essence of your story.", "projectName", "Your project is: "),
-    () => this.questioner("What's the main premise of this story?  In other words, what's the hook?  Or, in a Studio Hack kind of way... it's Story 'X' meets Story 'Y'.", "thePremise", "Your premise is: "),
-    () => this.questioner("Given your premise "+`${this.story.thePremise}`+" what realm of risks or stakes are most appealing?  Will the risks be: philosphical, religious, scientific, economic, political, revolutionary, aesethic, ethical, related to some type of sport or game, or something else entirely?", "row1risks", "The primary danger in your story will come from: "),
-    () => this.questioner("Considering "+`${this.story.row1risks}`+" what type of risk or stakes do you think should kick off the story?  Some examples of things that people fear are: Poverty, Failure, Criticism, Lost Love, Lost Liberty, Sickness, Old Age, and Death.  Try to make it specific, yet brief.", "row1col", "Remember, your entry level risk will be: "),
-    () => this.questioner("What would be a secondary risk to follow up your first one? "+`${this.story.row1col1}`, "row1col2", "Your second level of stakes will be: "),
+    () => this.questioner("What do you want to call this story?", "projectName", "Your project is: "),
+    () => this.questioner("What's the main premise of this story?", "thePremise", "Your premise is: "),
+    () => this.questioner("What realm of risks or stakes are most interesting?  For example: physical, personal, social, existential, or spiritual risks?", "row1risks", "The primary danger in your story will come from: "),
+    
+    () => this.questioner("What are the stakes that set your story into motion?", "row1col", "The stakes for act I will be: "),
+    () => this.questioner("What stakes are higher than the previous ones?", "row1col2", "Your second level of stakes will be: "),
     () => this.questioner("What would be even worse than "+`${this.story.row1col2}`, "row1col3", "Your middle level stakes are: "),
-    () => this.questioner("What escalation of risks or stakes would make your story more poignant?  Especially in the context of "+`${this.story.thePremise}`, "row1col4", "In order, your stakes so far are: "+`${row1col1}`+`${row1col2}`+`${row1col3}`+": "),
-    () => this.questioner()
+    () => this.questioner("What escalation of risks or stakes would make your story more poignant?  Especially in the context of "+`${this.story.thePremise}`, "row1col4", "... The stakes of your story are, for the beginning: "+`${this.story.row1col1}`+". The first escalation is: "+`${this.story.row1col2}`+". The middle of your story is"+`${this.story.row1col3}`+"and right before the climax: "),
+    () => this.questioner("What would be the final risks or stakes that best illustrate your premise and sum up"+`${this.story.row1risks}`+"?", "row1col5", "The context for the climax of your story is: " )
   
   ]
 
  
   async questionLoop() {
+    const story = this.story
     for (const question of this.questions) {
       await question()
       continue
@@ -50,9 +52,22 @@ class runTimeThing {
     }, { once: true});
   }
   
+  addEventListenersHere (arrayOfThingsThatNeedButtons) {
+    for (const thing of arrayOfThingsThatNeedButtons) {
+      console.log("this is :", thing.id)
+      thing.addEventListener('click', () => {
+      alert(thing.innerHTML)
+      })
+    }
+  }
+
+  makeThese = [switchboard.rowAlabel, switchboard.rowAcolumn1, switchboard.rowAcolumn2, switchboard.rowAcolumn3, switchboard.rowAcolumn4, switchboard.rowAcolumn5]
+
+
   mainLoop () {
     this.switchboard.compressedLook()
     this.turnItOn()
+    this.addEventListenersHere(this.makeThese)
     }
 }  
 
